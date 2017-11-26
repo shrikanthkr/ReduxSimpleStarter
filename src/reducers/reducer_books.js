cd520f93fd8a476308fe62e68dad1cba097390b1
shrikanthkr <shrikanth_kr@yahoo.com>
2017-11-26 13:50:32 +0530

    initial

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return [
+    {title: 'Javascript: Good parts', pages: 101},
+    {title: 'Harry Potter', pages: 200},
+    {title: 'Dark Tower', pages: 1000},
+    {title: 'VDOLOLOL', pages: 30}
+  ];
+}
